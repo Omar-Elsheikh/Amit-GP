8480c2777a50e78b9c01d471d870ece656c499e9
Omar-Elsheikh <75941267+Omar-Elsheikh@users.noreply.github.com>
2020-12-13 17:23:16 +0200

    Sub Main Microcontroller

---
+++ Sub Main/Sub Main/DIO_CFG.h
@@ -0,0 +1,27 @@
+/*
+ * DIO_CFG.h
+ *
+ * Created: 12/12/2020 07:34:15 PM
+ *  Author: Omar
+ */ 
+
+
+#ifndef DIO_CFG_H_
+#define DIO_CFG_H_
+
+
+#include "DIO.h"
+
+typedef struct {
+	DIO_DirTypes PinDir;
+	STD_LevelTypes PinLevel;
+}Dio_PinCFG;
+
+#define PINCOUNT 32
+
+void DIO_Init(void);
+
+
+
+
+#endif /* DIO_CFG_H_ */
---
+++ Sub Main/Sub Main/main.c
@@ -0,0 +1,29 @@
+/*
+ * Sub Main.c
+ *
+ * Created: 12/12/2020 07:21:16 PM
+ * Author : Omar
+ */ 
+
+#include <avr/io.h>
+#include "avr/interrupt.h"
+#include "STD_types.h"
+#include "DIO.h"
+#include "LCD.h"
+
+
+int main(void)
+{
+	DIO_Init();
+	SPI_Init('S');
+	LCD_Init();
+	LCD_clr();
+	
+	LCD_cmd(0x85);
+	LCD_StringPos("Smart Home",1,3);
+	LCD_StringPos("By O.Elsheikh",2,2);
+	while (1)	{				if( SPI_Receive() == 'o' )		{			DIO_write(DIO_channelC4,STD_low);			DIO_write(DIO_channelC5,STD_high);			DIO_write(DIO_channelC6,STD_low);		}						if( SPI_Receive() == 'e' )		{			DIO_write(DIO_channelC4,STD_high);			DIO_write(DIO_channelC5,STD_low);			DIO_write(DIO_channelC6,STD_high);		}					}
+
+
+}
+
